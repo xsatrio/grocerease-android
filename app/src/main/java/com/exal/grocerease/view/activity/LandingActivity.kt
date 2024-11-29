4087b5c17a797feba40358430c263302fa29f3b3
Satriooo <satriocahyo93@gmail.com>
2024-11-30 00:41:11 +0700

    Completed : A way to access intro activity.

--- app/src/main/java/com/exal/grocerease/view/activity/LandingActivity.kt
+++ app/src/main/java/com/exal/grocerease/view/activity/LandingActivity.kt
@@ -4,10 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.exal.grocerease.databinding.ActivityLandingBinding
+import com.exal.grocerease.helper.manager.IntroManager
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class LandingActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var introManager: IntroManager
 
     private var binding: ActivityLandingBinding? = null
     private val _binding get() = binding!!
@@ -24,6 +29,12 @@ class LandingActivity : AppCompatActivity() {
 
         _binding.registerButton.setOnClickListener {
             val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        _binding.helpBtn.setOnClickListener {
+            introManager.clearIntroFlag()
+            val intent = Intent(this, IntroActivity::class.java)
             startActivity(intent)
         }
     }
